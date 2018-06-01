86c57e2afd4dd473d6ea73770353a58d4829b802
whelks-chance <iharvey111@gmail.com>
2018-06-01 17:28:46 +0100

    max min mean stdev

--- dataimport/scripts/stop.py
+++ dataimport/scripts/stop.py
@@ -2,7 +2,9 @@ import json
 
 import os
 import pprint
+import statistics
 
+import sys
 from openpyxl.utils import get_column_letter
 from openpyxl.workbook import Workbook
 from openpyxl.reader.excel import load_workbook, InvalidFileException
@@ -15,6 +17,7 @@ class ReadStop:
         self.filename = ''
         self.all_data_file_idx = 0
         self.summary_count = 1
+        self.session_durations = []
 
     def read_stop_file(self, json_file):
         with open(json_file, 'r') as json_file_handle:
@@ -55,6 +58,13 @@ class ReadStop:
         session_start = json_blob['data'][0]['sessionStart']
         session_end = json_blob['data'][0]['sessionEnd']
         previous_trial_start = None
+
+        trial_durations = []
+        stop_signal_durations = []
+        stimulus_durations = []
+        signal_stop_delays = []
+        interTrialIntervals = []
+        stim_stop_sig_offset_diffs = []
 
         max_round = 0
         round_trial_counts = {}
@@ -109,6 +119,7 @@ class ReadStop:
                 if trial['trialEnd'] and trial['trialStart']:
                     trial_duration = trial['trialEnd'] - trial['trialStart']
                     sheet.cell(column=len(col_headers) + 1, row=s_idx+3, value=trial_duration)
+                    trial_durations.append(trial_duration)
 
             # Stop signal duration: stopSignalOffset - stopSignalOnset
             sheet.cell(column=len(col_headers) + 2, row=2, value="Stop Signal Duration")
@@ -116,12 +127,14 @@ class ReadStop:
                 if trial['stopSignalOffset'] and trial['stopSignalOnset']:
                     stop_signal_duration = trial['stopSignalOffset'] - trial['stopSignalOnset']
                     sheet.cell(column=len(col_headers) + 2, row=s_idx+3, value=stop_signal_duration)
+                    stop_signal_durations.append(stop_signal_duration)
 
             # Stimulus duration: stimulusOffset - stimulusOnset
             sheet.cell(column=len(col_headers) + 3, row=2, value="Stimulus Duration")
             if 'stimulusOffset' in trial and 'stimulusOnset' in trial:
                 stimulus_duration = trial['stimulusOffset'] - trial['stimulusOnset']
                 sheet.cell(column=len(col_headers) + 3, row=s_idx+3, value=stimulus_duration)
+                stimulus_durations.append(stimulus_duration)
 
             # Difference between signal onset and stop signal delay: stopSignalOnset - stopSignalDelay
             sheet.cell(column=len(col_headers) + 4, row=2, value="stopSignalOnset - stopSignalDelay")
@@ -129,6 +142,16 @@ class ReadStop:
                 if trial['stopSignalOnset'] and trial['stopSignalDelay']:
                     signal_stop_delay = trial['stopSignalOnset'] - trial['stopSignalDelay']
                     sheet.cell(column=len(col_headers) + 4, row=s_idx + 3, value=signal_stop_delay)
+                    signal_stop_delays.append(signal_stop_delay)
+
+            # Duration between signal offset and stimulus offset: stimulusOffset - stopSignalOffset
+            sheet.cell(column=len(col_headers) + 5, row=2, value="stimulusOffset - stopSignalOffset")
+            if 'stimulusOffset' in trial and 'stopSignalOffset' in trial:
+                if trial['stimulusOffset'] and trial['stopSignalOffset']:
+                    # TODO this is zero apparently...
+                    stim_stop_sig_offset_diff = trial['stimulusOffset'] - trial['stopSignalOffset']
+                    sheet.cell(column=len(col_headers) + 5, row=s_idx + 3, value=stim_stop_sig_offset_diff)
+                    stim_stop_sig_offset_diffs.append(stim_stop_sig_offset_diff)
 
             # interTrialInterval
             # Duration between trials: trialOnsetB - trialOnsetA,
@@ -139,19 +162,12 @@ class ReadStop:
                 if trial['trialStart']:
                     if previous_trial_start:
                         interTrialInterval = trial['trialStart'] - previous_trial_start
-                        sheet.cell(column=len(col_headers) + 5, row=2,
+                        sheet.cell(column=len(col_headers) + 6, row=2,
                                    value="interTrialInterval")
-                        sheet.cell(column=len(col_headers) + 5, row=s_idx + 3,
+                        sheet.cell(column=len(col_headers) + 6, row=s_idx + 3,
                                    value=interTrialInterval)
+                        interTrialIntervals.append(interTrialInterval)
                     previous_trial_start = trial['trialStart']
-
-            # Duration between signal offset and stimulus offset: stimulusOffset - stopSignalOffset
-            if 'stimulusOffset' in trial and 'stopSignalOffset' in trial:
-                if trial['stimulusOffset'] and trial['stopSignalOffset']:
-                    # TODO this is zero apparently
-                    signal_stimulus_offsets = trial['stimulusOffset'] - trial['stopSignalOffset']
-                    sheet.cell(column=len(col_headers) + 6, row=2, value="stimulusOffset - stopSignalOffset")
-                    sheet.cell(column=len(col_headers) + 6, row=s_idx + 3, value=signal_stimulus_offsets)
 
             if 'trialType' in trial:
                 # Global store of numbers of trialType (STOP, GO) by Block
@@ -213,8 +229,10 @@ class ReadStop:
                 self.store_summary_key_value('Response High', 'N/A')
 
             sheet2.cell(column=4, row=2, value='Session Duration')
-            sheet2.cell(column=4, row=3, value=(session_end - session_start))
-            self.store_summary_key_value('Session Duration', (session_end - session_start))
+            session_duration = (session_end - session_start)
+            self.session_durations.append(session_duration)
+            sheet2.cell(column=4, row=3, value=session_duration)
+            self.store_summary_key_value('Session Duration', session_duration)
 
             sheet2.cell(column=5, row=2, value='Session Event Count')
             sheet2.cell(column=5, row=3, value=len(session_data))
@@ -266,9 +284,82 @@ class ReadStop:
                         self.store_summary_key_value("{}_block_{} / {}_total".format(key, block, key),
                                                      (self.trial_type_counts['blocks'][block][key]/self.trial_type_counts['total'][key]))
 
+            if len(trial_durations):
+                min_trial_duration = min(trial_durations)
+                max_trial_duration = max(trial_durations)
+                mean_trial_duration = statistics.mean(trial_durations)
+                trial_duration_stdev = statistics.stdev(trial_durations)
+
+                self.store_summary_key_value('Min Trial Duration', min_trial_duration)
+                self.store_summary_key_value('Max Trial Duration', max_trial_duration)
+                self.store_summary_key_value('Mean Trial Duration', mean_trial_duration)
+                self.store_summary_key_value('Trial Duration Std Dev', trial_duration_stdev)
+
+            if len(stop_signal_durations):
+                min_stop_signal_duration = min(stop_signal_durations)
+                max_stop_signal_duration = max(stop_signal_durations)
+                mean_stop_signal_duration = statistics.mean(stop_signal_durations)
+                stop_signal_duration_stdev = statistics.stdev(stop_signal_durations)
+
+                self.store_summary_key_value('Min Stop Signal Duration', min_stop_signal_duration)
+                self.store_summary_key_value('Max Stop Signal Duration', max_stop_signal_duration)
+                self.store_summary_key_value('Mean Stop Signal Duration', mean_stop_signal_duration)
+                self.store_summary_key_value('Stop Signal Duration Std Dev', stop_signal_duration_stdev)
+
+            if len(stimulus_durations):
+                min_stimulus_duration = min(stimulus_durations)
+                max_stimulus_duration = max(stimulus_durations)
+                mean_stimulus_duration = statistics.mean(stimulus_durations)
+                stimulus_duration_stdev = statistics.stdev(stimulus_durations)
+
+                self.store_summary_key_value('Min Stimulus Duration', min_stimulus_duration)
+                self.store_summary_key_value('Max Stimulus Duration', max_stimulus_duration)
+                self.store_summary_key_value('Mean Stimulus Duration', mean_stimulus_duration)
+                self.store_summary_key_value('Stop Stimulus Std Dev', stimulus_duration_stdev)
+
+            if len(signal_stop_delays):
+                min_signal_stop_delay = min(signal_stop_delays)
+                max_signal_stop_delay = max(signal_stop_delays)
+                mean_signal_stop_delay = statistics.mean(signal_stop_delays)
+                signal_stop_delay_stdev = statistics.stdev(signal_stop_delays)
+
+                self.store_summary_key_value('Min Signal Stop Delay', min_signal_stop_delay)
+                self.store_summary_key_value('Max Signal Stop Delay', max_signal_stop_delay)
+                self.store_summary_key_value('Mean Signal Stop Delay', mean_signal_stop_delay)
+                self.store_summary_key_value('Signal Stop Delay Std Dev', signal_stop_delay_stdev)
+
+            if len(interTrialIntervals):
+                min_interTrialInterval = min(interTrialIntervals)
+                max_interTrialInterval = max(interTrialIntervals)
+                mean_interTrialInterval = statistics.mean(interTrialIntervals)
+                interTrialInterval_stdev = statistics.stdev(interTrialIntervals)
+
+                self.store_summary_key_value('Min InterTrial Interval', min_interTrialInterval)
+                self.store_summary_key_value('Max InterTrial Interval', max_interTrialInterval)
+                self.store_summary_key_value('Mean InterTrial Interval', mean_interTrialInterval)
+                self.store_summary_key_value('InterTrial Interval Std Dev', interTrialInterval_stdev)
+
+            if len(stim_stop_sig_offset_diffs):
+                min_stim_stop_sig_offset_diff = min(stim_stop_sig_offset_diffs)
+                max_stim_stop_sig_offset_diff = max(stim_stop_sig_offset_diffs)
+                mean_stim_stop_sig_offset_diff = statistics.mean(stim_stop_sig_offset_diffs)
+                stim_stop_sig_offset_diff_stdev = statistics.stdev(stim_stop_sig_offset_diffs)
+
+                self.store_summary_key_value('Min Stimulus - Stop Signal Offset Difference',
+                                             min_stim_stop_sig_offset_diff)
+                self.store_summary_key_value('Max Stimulus - Stop Signal Offset Difference',
+                                             max_stim_stop_sig_offset_diff)
+                self.store_summary_key_value('Mean Stimulus - Stop Signal Offset Difference',
+                                             mean_stim_stop_sig_offset_diff)
+                self.store_summary_key_value('Stimulus - Stop Signal Offset Difference Std Dev',
+                                             stim_stop_sig_offset_diff_stdev)
+
             self.resize_sheet_columns(sheet2, 1)
         except Exception as e1:
             print('Data Summary error', e1)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
 
         parent = os.path.dirname(os.path.abspath(output_filepath))
         new_folder = os.path.join(parent, folder)
@@ -305,41 +396,14 @@ class ReadStop:
                         if 'sessionEvents' in a['data'][0]:
                             task_count += len(a['data'][0]['sessionEvents'])
 
-                # if a['type'] == 'STOP':
-                #     self.output_srgame_spreadsheet(
-                #         a,
-                #         folder=new_folder_name
-                #     )
-                #
-                # elif a['type'] == 'NASTOP':
-                #     self.output_srgame_spreadsheet(
-                #         a,
-                #         folder=new_folder_name
-                #     )
-                #
-                # elif a['type'] == 'GSTOP':
-                #     self.output_srgame_spreadsheet(
-                #         a,
-                #         folder=new_folder_name
-                #     )
-                #
-                # elif a['type'] == 'GRESTRAINT':
-                #     self.output_srgame_spreadsheet(
-                #         a,
-                #         folder=new_folder_name
-                #     )
-                #
-                # elif a['type'] == 'DOUBLE':
-                #     self.output_srgame_spreadsheet(
-                #         a,
-                #         folder=new_folder_name
-                #     )
-
                 if a['type'] == 'virtual-supermarket-selected':
                     print('virtual-supermarket-selected idx', idx)
                 else:
+
                     # print('\n', type(a['data']), a['data'], '\n')
                     if 'data' in a and isinstance(a['data'], list) and len(a['data']) and 'gameSessionID' in a['data'][0]:
+
+                        # It's a game - we'll inspect further
                         self.output_srgame_spreadsheet(
                             a,
                             folder=new_folder_name
@@ -362,6 +426,14 @@ class ReadStop:
         sheet.cell(column=2,
                    row=self.summary_count,
                    value=value)
+
+        dim = sheet.column_dimensions[get_column_letter(1)]
+        if not dim.width or len(str(key)) > dim.width:
+            dim.width = len(str(key))
+
+        dim = sheet.column_dimensions[get_column_letter(2)]
+        if not dim.width or len(str(value)) > dim.width:
+            dim.width = len(str(value))
 
     def store_trial_round_game_type(self, trial):
         # print('\n\n', trial)
@@ -408,6 +480,24 @@ class ReadStop:
                 else:
                     self.trial_type_counts['blocks'][block_number]['NON_HEALTHY'] += 1
 
+    def output_global_stats(self):
+        print('Called output globals.xlsx')
+        wb = Workbook()
+        sheet = wb.active
+        sheet.title = 'Data Output'
+
+        if len(self.session_durations):
+            sheet.cell(column=1, row=2, value='avg')
+            sheet.cell(column=2, row=2, value=sum(self.session_durations) / len(self.session_durations))
+
+            sheet.cell(column=1, row=2, value='avg')
+            sheet.cell(column=2, row=2, value=sum(self.session_durations) / len(self.session_durations))
+
+            sheet.cell(column=1, row=2, value='avg')
+            sheet.cell(column=2, row=2, value=sum(self.session_durations) / len(self.session_durations))
+
+        wb.save('globals.xlsx')
+
 
 if __name__ == '__main__':
     rs = ReadStop()
@@ -422,3 +512,4 @@ if __name__ == '__main__':
 
     all_data_file = '020518.json'
     rs.sort_all_data(os.path.join(data_dir, all_data_file))
+    rs.output_global_stats()
