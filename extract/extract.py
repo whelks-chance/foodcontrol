49ba1ab616b051f155866230442d8c7e385033c5
Jeffrey Morgan <DrMorganJeffrey@gmail.com>
2018-08-22 10:52:31 +0100

    Refactor filename creation.

--- extract/extract.py
+++ extract/extract.py
@@ -19,11 +19,12 @@ class Extractor:
 
     def extract_from_json(self, json_array, json_csv_path):
         for extractor in self.extractor_factory.extractors:
-            print('\n{}'.format(extractor.type))
+            print('\nEXTRACTOR: {}'.format(extractor.type))
             self.extract_row_type(json_array, extractor, json_csv_path)
 
     def extract_row_type(self, json_array, extractor, json_csv_path):
         if not self.has_row_to_extract(json_array, extractor):
+            print('no rows to extract with: ', extractor.type)
             return
 
         output_filename = json_csv_path / '{}.csv'.format(extractor.type)
@@ -33,6 +34,7 @@ class Extractor:
             for row in json_array:
                 row = KeypathDict(row)
                 if extractor.process(row):
+                    print(extractor.row_values())
                     csv_writer.writerow(extractor.row_values())
 
     @staticmethod
@@ -48,7 +50,7 @@ if __name__ == '__main__':
 
     json_filenames = [
         # '020518.json', #  Old format?
-        # '060618.json',
+        '060618.json',
         '070618.json',
     ]
 
@@ -56,12 +58,15 @@ if __name__ == '__main__':
     def create_folder(path):
         path.mkdir(parents=True, exist_ok=True)
 
+    def filename_without_extension(filename):
+        (base_filename, _) = os.path.splitext(os.path.basename(json_filename))
+        return base_filename
+
 
     csv_path = Path('./csv')
     create_folder(csv_path)
     for json_filename in json_filenames:
-        (base_filename, _) = os.path.splitext(os.path.basename(json_filename))
-        json_csv_path = csv_path / base_filename
+        json_csv_path = csv_path / filename_without_extension(json_filename)
         create_folder(json_csv_path)
         filename = food_control_path / json_filename
         extractor.extract_from_json_filename(filename, json_csv_path)
