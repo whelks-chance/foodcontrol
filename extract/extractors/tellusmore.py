aa775bd1c9353da517bd351d9ded376ba1024b8c
Jeffrey Morgan <DrMorganJeffrey@gmail.com>
2018-08-31 16:38:58 +0100

    Add (initial) question extraction.

--- extract/extractors/tellusmore.py
+++ extract/extractors/tellusmore.py
@@ -2,8 +2,16 @@ import re
 
 from .dataextractor import DataExtractor
 
+from keypathdict import KeypathDict
+
+
+def irange(start, end):
+    return range(start, end + 1)
+
 
 class AbstractQuestionnaireExtractor:
+
+    values = {}
 
     @staticmethod
     def can_process_data_with_pattern(data, pattern):
@@ -13,8 +21,41 @@ class AbstractQuestionnaireExtractor:
                 return True
         return False
 
+    def clear(self):
+        """Reset common value and calculation stores. Override to to reset type-specific value and calculation stores"""
+        self.values.clear()
+
+    def extract(self, data):
+        """Store the column value for each keypath"""
+        self.clear()
+        # print(data)
+        for freq_major in irange(1, 5):
+            for freq_minor in irange(1, 12):
+                freq_key = 'FREQ{}-{}'.format(freq_major, freq_minor)
+                # print(freq_key)
+                try:
+                    freq_data = data[freq_key]
+                    print(freq_key, '->', freq_data)
+                    for column_name, keypath in self.fields:
+                        print(column_name, keypath)
+                        value = data.get_keypath_value(keypath)
+                        self.values[column_name] = value
+                except KeyError:
+                    pass
+                    # print(freq_key, '->', 'None')
+
 
 class FreqExtractor(AbstractQuestionnaireExtractor):
+
+    fields = (
+        ('FE', 'answers.FE.answer', ),
+        ('FC', 'answers.FC.answer', ),
+        ('FC Slider Value', 'answers.FC.sliderValue', ),
+        ('ID', 'VsmInfo.id', ),
+        ('Type', 'VsmInfo.type', ),
+        ('Selected', 'VsmInfo.selected', ),
+        ('Time On Question', 'timeOnQuestion', ),
+    )
 
     def can_process_data(self, data):
         return self.can_process_data_with_pattern(data, r'FREQ[12345]-1?\d')
@@ -128,17 +169,19 @@ class TellUsMoreDataExtractor(DataExtractor):
         RESTRExtractor(),
     ]
 
-    def get_question_extractor(self, row):
+    def get_question_extractor(self, data):
         for question_extractor in self.question_extractors:
-            data = row['data']
             if question_extractor.can_process_data(data):
                 return question_extractor
         return None
 
     def extract(self, row):
-        question_extractor = self.get_question_extractor(row)
+        data = KeypathDict(row['data'])
+        question_extractor = self.get_question_extractor(data)
         if question_extractor:
             print('row is', type(question_extractor).__name__)
+            question_extractor.extract(data)
+            print('values =', question_extractor.values)
         else:
             print('no QE for row:', row['type'])
 
