440090b9c6755df44111743c176e39692e902785
Jeffrey Morgan <DrMorganJeffrey@gmail.com>
2018-10-10 15:52:39 +0100

    Add new JSON file.

--- extract/extract.py
+++ extract/extract.py
@@ -58,7 +58,8 @@ if __name__ == '__main__':
         '070618.json',
         '200818.json',
         '040918.json',
-        '260918.json'
+        '260918.json',
+        '010618_to_040918.json',
     ]
 
     def create_folder(path):
