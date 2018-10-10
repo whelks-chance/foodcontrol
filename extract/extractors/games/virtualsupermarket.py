fb5d353757e77ca76f640a8c4e67137db89f8e41
Jeffrey Morgan <DrMorganJeffrey@gmail.com>
2018-10-10 14:58:10 +0100

    Extract data from the virtual supermarket game.

--- extract/extractors/games/virtualsupermarket.py
+++ extract/extractors/games/virtualsupermarket.py
@@ -2,19 +2,54 @@ from collections import defaultdict
 
 from .gamedataextractor import GameDataExtractor
 
+# from utils import irange
+
+import pprint
+
 
 class VirtualSupermarketDataExtractor(GameDataExtractor):
 
     type = 'virtual-supermarket-selected'
 
-    # derived_fields = [
-    #     (None, 'Shop Number', 'shop_number'),
-    #     (None, 'Shop Name', 'shop_name'),
-    #     (None, 'Shop Type', 'shop_type'),
-    #     (None, 'Item ID', 'item_id'),
-    #     (None, 'Item Name', 'item_name'),
-    #     (None, 'Item Selected', 'item_selected'),
-    # ]
+    common_shop_keypaths = [
+        ('shop', 'Shop'),
+        ('type', 'Type'),
+        ('name', 'Name')
+    ]
+
+    item_keypaths = [
+        ('id', 'Item ID'),
+        ('name', 'Item Name'),
+        ('selected', 'Item Selected')
+    ]
+
+    def get_value_keypaths_for_naming_columns(self):
+        return self.common_shop_keypaths + self.item_keypaths
+
+    def extract_shop_rows(self, common_values, common_shop_values, shop_data):
+        rows = []
+        for item in shop_data['items']:
+            show_values = self.extract_values_with_keypaths(self.item_keypaths, [], item)
+            show_values.update(common_shop_values)
+            show_values.update(common_values)
+            rows.append(show_values)
+        return rows
+
+    def extract_values(self, data):
+        # print('VIRTUAL SUPERMARKET')
+        common_values = self.extract_values_with_keypaths(self.get_common_keypaths(), [], data)
+        # pprint.pprint(common_values)
+        rows = []
+        for shop_number in ['shop1', 'shop2']:
+            shop_data = data['data'][shop_number]
+            shop_data['shop'] = shop_number
+            common_shop_values = self.extract_values_with_keypaths(self.common_shop_keypaths, [], shop_data)
+            # pprint.pprint(common_shop_values)
+            shop_rows = self.extract_shop_rows(common_values, common_shop_values, shop_data)
+            # print('\t{}'.format(shop_number), shop_rows)
+            rows += shop_rows
+            # pprint.pprint(rows)
+        return rows
 
     selected_value_count = defaultdict(int)
 
