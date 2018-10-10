aa8172cf52fafb0593ec65c3c1ab716e5ff4e424
Jeffrey Morgan <DrMorganJeffrey@gmail.com>
2018-10-10 15:32:14 +0100

    Add key to value comment.

--- extract/extractors/games/virtualsupermarket.py
+++ extract/extractors/games/virtualsupermarket.py
@@ -36,7 +36,7 @@ class VirtualSupermarketDataExtractor(GameDataExtractor):
         rows = []
         for shop_number in ['shop1', 'shop2']:
             shop_data = data['data'][shop_number]
-            shop_data['shop'] = shop_number
+            shop_data['shop'] = shop_number  # add this key-encoded data as a value to extract it with a keypath
             common_shop_values = self.extract_values_with_keypaths(self.common_shop_keypaths, [], shop_data)
             shop_rows = self.extract_shop_rows(common_values, common_shop_values, shop_data)
             rows += shop_rows
