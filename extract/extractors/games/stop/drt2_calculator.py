8bc10331e428cee6d11300b26fe72e4e9fdd738a
Jeffrey Morgan <DrMorganJeffrey@gmail.com>
2018-12-05 14:56:02 +0000

    Add DRT2 calculation.

--- extract/extractors/games/stop/drt2_calculator.py
+++ extract/extractors/games/stop/drt2_calculator.py
@@ -1,21 +1,51 @@
+import statistics
+
 from .abstract_stop_evaluator import AbstractStopEvaluator
 
-from utils import get_session_events
+from utils import get_session_events, numericify
 
 
 class DRT2Calculator(AbstractStopEvaluator):
 
     def __init__(self):
-        self.ideal_drt2 = None
-        self.actual_drt2 = None
+        self.ideal_drt2_stats = None
+        self.actual_drt2_stats = None
 
     def evaluate(self, row):
-        # TODO: Add calculation
         session_events = get_session_events(row)
-        self.ideal_drt2 = 0
-        self.actual_drt2 = 0
+        ideal_drt2s = []
+        actual_drt2s = []
+        for session_event in session_events:
+            second_tap_response_start = numericify(session_event['secondTapResponseStart'])
+            # Ideal DRT2
+            stop_signal_delay = numericify(session_event['stopSignalDelay'])
+            ideal_drt2 = second_tap_response_start - stop_signal_delay
+            ideal_drt2s.append(ideal_drt2)
+            # Actual DRT2
+            stop_signal_onset = numericify(session_event['stopSignalOnset'])
+            actual_drt2 = second_tap_response_start - stop_signal_onset
+            actual_drt2s.append(actual_drt2)
+        # Stats
+        self.ideal_drt2_stats = {
+            'min': min(ideal_drt2s),
+            'max': max(ideal_drt2s),
+            'mean': statistics.mean(ideal_drt2s),
+        }
+        self.actual_drt2_stats = {
+            'min': min(actual_drt2s),
+            'max': max(actual_drt2s),
+            'mean': statistics.mean(actual_drt2s),
+        }
 
     def populate_spreadsheet(self, spreadsheet):
         spreadsheet.select_sheet('DRT2')
-        spreadsheet.set_values(['Ideal DRT2', self.ideal_drt2])
-        spreadsheet.set_values(['Actual DRT2', self.actual_drt2])
+
+        spreadsheet.set_values(['Ideal DRT2'])
+        spreadsheet.set_values(['Min', self.ideal_drt2_stats['min']])
+        spreadsheet.set_values(['Max', self.ideal_drt2_stats['max']])
+        spreadsheet.set_values(['Mean', self.ideal_drt2_stats['mean']], advance_by_rows=2)
+
+        spreadsheet.set_values(['Actual DRT2'])
+        spreadsheet.set_values(['Min', self.actual_drt2_stats['min']])
+        spreadsheet.set_values(['Max', self.actual_drt2_stats['max']])
+        spreadsheet.set_values(['Mean', self.actual_drt2_stats['mean']])
--- extract/extractors/games/stop/ssrt_calculator.py
+++ extract/extractors/games/stop/ssrt_calculator.py
@@ -39,8 +39,8 @@ class SSRTCalculator(AbstractStopEvaluator):
         self.mean_stop_signal_onset = stop_signal_onset_total / len(session_events)
         self.mean_ideal_ssrt = mean_tap_response_start - self.mean_stop_signal_delay  # What the SSRT should be
         self.mean_actual_ssrt = mean_tap_response_start - self.mean_stop_signal_onset  # What the SSRT actually is
-        print('\n IDEAL MEAN SSRT:', self.mean_ideal_ssrt)
-        print('ACTUAL MEAN SSRT:', self.mean_actual_ssrt)
+        # print('\n IDEAL MEAN SSRT:', self.mean_ideal_ssrt)
+        # print('ACTUAL MEAN SSRT:', self.mean_actual_ssrt)
 
     def calculate_integration_ssrt(self, session_events):
         go_trial_count = 0
@@ -65,24 +65,25 @@ class SSRTCalculator(AbstractStopEvaluator):
         go_trial_tap_response_starts.sort()
         stop_signal_trial_probability = stop_trail_with_response_count / stop_trial_count
         n = go_trial_count * stop_signal_trial_probability
-        print('GO TRIALS TRSs:', go_trial_tap_response_starts)
-        print('GO TRIALS TRSs count:', len(go_trial_tap_response_starts))
-        print('INCORRECT STOP TRIALS:', incorrect_stop_trials_count)
-        print('p(stop signal trial):', stop_signal_trial_probability)
-        print('n:', n)
+        # print('GO TRIALS TRSs:', go_trial_tap_response_starts)
+        # print('GO TRIALS TRSs count:', len(go_trial_tap_response_starts))
+        # print('INCORRECT STOP TRIALS:', incorrect_stop_trials_count)
+        # print('p(stop signal trial):', stop_signal_trial_probability)
+        # print('n:', n)
         n = int(n) - 1
         assert n >= 0
         nth = go_trial_tap_response_starts[int(n)]
-        print('nth:', nth)
+        # print('nth:', nth)
         self.ideal_integration_ssrt = nth - self.mean_stop_signal_delay
         self.actual_integration_ssrt = nth - self.mean_stop_signal_onset
-        print('ideal_integration_ssrt:', self.ideal_integration_ssrt)
-        print('actual_integration_ssrt:', self.actual_integration_ssrt)
-        # assert False
+        # print('ideal_integration_ssrt:', self.ideal_integration_ssrt)
+        # print('actual_integration_ssrt:', self.actual_integration_ssrt)
 
     def populate_spreadsheet(self, spreadsheet):
         spreadsheet.select_sheet('SSRT')
+
         spreadsheet.set_values(['Ideal Mean SSRT', self.mean_ideal_ssrt])
         spreadsheet.set_values(['Actual Mean SSRT', self.mean_actual_ssrt])
+
         spreadsheet.set_values(['Ideal Integration SSRT', self.ideal_integration_ssrt])
         spreadsheet.set_values(['Actual Integration SSRT', self.actual_integration_ssrt])
